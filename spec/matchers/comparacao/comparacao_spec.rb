44aa54e267ee9c2e1ab01531e7fbf08b7d88f441
Kaio Masaru Nakazono <kmn1405.cg@gmail.com>
2021-09-13 19:20:23 +0000

    matchers de comparacao equal/be/eql/eq

---
+++ spec/matchers/comparacao/comparacao_spec.rb
@@ -0,0 +1,26 @@
+describe 'Matchers de comparacao' do
+   it "#equal: testa se dois objetos são iguais" do 
+      x = "ruby"
+      y = "ruby"
+      expect(x).not_to equal(y)
+   end
+
+   it "#be: testa se dois objetos são iguais" do 
+      x = "ruby"
+      y = "ruby"
+      expect(x).not_to be(y)
+      expect(x).to be(x)
+   end
+
+   it "#eql: teste se dois objetos possuem o mesmo conteúdo" do 
+      x = "ruby"
+      y = "ruby"
+      expect(x).to eql(y)
+   end
+
+   it "#eq: teste se dois objetos possuem o mesmo conteúdo" do 
+      x = "ruby"
+      y = "ruby"
+      expect(x).to eq(y)
+   end
+end
